bf6471eac05c165016675098d0dd3bafc974e58c
Mikheil Makasarashvili <makasarashvilimikheil@gmail.com>
2022-10-30 11:50:54 +0400

    first commit

---
+++ src/examples/1-first-request.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+// limit, if 429 wait for 15 min and try again
+const url = 'https://course-api.com/react-store-products';
+
+const FirstRequest = () => {
+  //create a fetch function
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  // get data with useEffect
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h2 className='text-center'>first request</h2>;
+};
+export default FirstRequest;
